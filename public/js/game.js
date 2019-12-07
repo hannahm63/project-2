311784b9f66208f531c5762e55c89a651fc17386
Killoran Lehenbauer <k.lehenbauer13@gmail.com>
2019-12-07 11:43:21 -0700

    Added Hannah's changes

--- public/js/game.js
+++ public/js/game.js
@@ -1,14 +1,12 @@
 $(document).ready(function() {
-    $("#addreview-btn").on("click", function(event) {
-        event.preventDefault();
+  $("#addreview-btn").on("click", function(event) {
+    event.preventDefault();
 
-        $.ajax({
-            url: "/api/games",
-            method: "POST"
-        })
-        .then(function(data) {
-            console.log(data);
-            
-        })
-    })
+    $.ajax({
+      url: "/api/games",
+      method: "POST"
+    }).then(function(data) {
+      console.log(data);
+    });
+  });
 });
