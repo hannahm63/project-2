287bd25ae7bf7fc0ce913256b955a0e1e8abe222
Killoran Lehenbauer <k.lehenbauer13@gmail.com>
2019-12-07 10:49:38 -0700

    Finished app.post, started ajax post

--- public/js/game.js
+++ public/js/game.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
-    $("#addreview-btn").on("click", function() {
-        
+    $("#addreview-btn").on("click", function(event) {
+        event.preventDefault();
+
+        $.ajax({
+            url: "/api/games",
+            method: "POST"
+        })
+        .then(function(data) {
+            console.log(data);
+            
+        })
     })
 });
